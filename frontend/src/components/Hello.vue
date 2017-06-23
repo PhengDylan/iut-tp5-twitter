aece2d6efd18030472b46c68f97bb7df562d3fe3
PhengDylan <ph3ng.dylan@gmail.com>
2017-06-23 11:26:30 +0200

    Etape 2: Router et création de la Timeline

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -2,5 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Bienvenue sur Twitter</h2>
+    <ul>
+    <li><router-link to="/timeline">Timeline</router-link></li>
+    </ul>
   </div>
 </template>
