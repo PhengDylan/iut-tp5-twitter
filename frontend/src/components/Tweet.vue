d5dccf88e3dd896abc61f30d45fcc9d594192af6
PhengDylan <ph3ng.dylan@gmail.com>
2017-06-23 13:45:19 +0200

    Rajout du component Tweet

---
+++ frontend/src/components/Tweet.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Tweet">
+
+    <ul>
+    {{ tweets.Auteur }} : {{ tweets.tweet }}
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tweet',
+  props: ['tweet']
+  }
+}
+</script>
+
+<style scoped>
+
+}
+ </style>
