353739ef507b2b7e86d490e0c4e9a5e5bb196bec
PhengDylan <ph3ng.dylan@gmail.com>
2017-06-23 11:38:02 +0200

    Etape 3: Ajout de données et visualisation des données

--- frontend/src/components/Timeline.vue
+++ frontend/src/components/Timeline.vue
@@ -1,4 +1,20 @@
 <template>
   <div class="Timeline">
+    <h1>{{ tweets }}</h1>
+    <ul>
+    <li><v-for="tweet in tweets"></li>
+    </ul>
+
   </div>
 </template>
+
+<script>
+export default {
+  name: 'timeline',
+  data () {
+    return {
+      tweets: ['tweet 1', 'tweet 2', 'tweet 3']
+    }
+  }
+}
+</script>
